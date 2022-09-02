ef79ac3fd46837de7624ac1e130d3ff429a8d77c
sifat <Sifatami596@gmail.com>
2022-09-03 02:23:23 +0600

    header added

--- js/app.js
+++ js/app.js
@@ -1,19 +1,24 @@
+const loadNews = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    displayNews(data.data.news_category);
+};
 
+const displayNews = (newses) => {
+    console.log(newses);
+    const newsContainer = document.getElementById("news-container");
+    console.log(newsContainer);
 
+    newses.forEach((news) => {
+        console.log(news.category_name);
+        const newsDiv = document.createElement("div");
+        newsDiv.innerHTML = `
+		<a class="nav-link text-white" href="#">${news.category_name}</a>
+		`;
+        newsContainer.appendChild(newsDiv);
+    });
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+loadNews();
